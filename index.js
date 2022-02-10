2a9269a0cdf8dd1e4746cdfcab99e88c35c691e4
Alisa Gezalova <alisa.gezalova@gmail.com>
2022-02-09 22:25:28 -0700

    commit tree e2699f6e7f6a97e503543d57416c090412b2c2c8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat() {
+    const newCats = [...cats, "Broom"]
+    return newCats
+}
+
+function prependCat() {
+    const newCats2 = ["Arnold", ...cats]
+    return newCats2
+}
+
+function removeLastCat() {
+    const newCats3 = cats.slice(0, -1)
+    return newCats3
+}
+
+function removeFirstCat() {
+    const newCats4 = cats.slice(1)
+    return newCats4
+}
